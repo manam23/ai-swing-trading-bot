3f2794c33ec231a2c6686574f981cdb5a74e8023
JAISANKAR <jaisankar6789@gmail.com>
2026-05-27 07:07:42 +0530

    Added missing init files

--- app/scanner/run_scanner.py
+++ app/scanner/run_scanner.py
@@ -1,6 +1,11 @@
 import schedule
 import time
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from app.scanner.data_fetcher import fetch_stock_data
 from app.scanner.data_fetcher import fetch_stock_data
 
 from app.indicators.ema_indicator import calculate_ema
